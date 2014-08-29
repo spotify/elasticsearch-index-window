3f3ef4e64731d1500589cc2ee33bac117bb7224d
Uwe Dauernheim <uwe@dauernheim.net>
2014-08-29 15:15:58 +0200

    Apply Google Code Style scheme
    Also remove Lombok plugin.
    
    Change-Id: I2abe84c8e74a4a80542f2e729e7b5ddfbb808877

--- src/main/java/com/spotify/elasticsearch/plugins/indexwindow/IndexWindow.java
+++ src/main/java/com/spotify/elasticsearch/plugins/indexwindow/IndexWindow.java
@@ -16,57 +16,40 @@
 
 package com.spotify.elasticsearch.plugins.indexwindow;
 
-import java.util.Date;
-
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@ToString
-@EqualsAndHashCode
 public class IndexWindow {
 
-    @Getter
-    @Setter
-    private String indexPrefix;
+  private String indexPrefix;
+  private String dateFormat;
+  private int keep;
+  private long checkInterval;
 
-    @Getter
-    @Setter
-    private String dateFormat;
+  /**
+   * @param indexPrefix   The prefix of indices to be windowed
+   * @param dateFormat    The date format of the postfix timestamp
+   * @param keep          Number of recent indices to be kept, i.e., size of the window
+   * @param checkInterval How often to check for deprecated (out-of-window) indices. The unit is milliseconds.
+   */
+  public IndexWindow(String indexPrefix, String dateFormat, int keep, long checkInterval) {
+    super();
+    this.indexPrefix = indexPrefix;
+    this.dateFormat = dateFormat;
+    this.keep = keep;
+    this.checkInterval = checkInterval;
+  }
 
-    @Getter
-    @Setter
-    private int keep;
+  public String getIndexPrefix() {
+    return indexPrefix;
+  }
 
-    @Getter
-    @Setter
-    private long checkInterval;
+  public String getDateFormat() {
+    return dateFormat;
+  }
 
-    public IndexWindow() {
-    }
+  public int getKeep() {
+    return keep;
+  }
 
-    /**
-     * @param indexPrefix
-     *            The prefix of indices to be windowed
-     * @param dateFormat
-     *            The date format of the postfix timestamp
-     * @param keep
-     *            Number of recent indices to be kept, i.e., size of the window
-     * @param checkInterval
-     *            How often to check for deprecated (out-of-window) indices. The
-     *            unit is milliseconds.
-     */
-    public IndexWindow(String indexPrefix, String dateFormat, int keep,
-            long checkInterval) {
-        super();
-        this.indexPrefix = indexPrefix;
-        this.dateFormat = dateFormat;
-        this.keep = keep;
-        this.checkInterval = checkInterval;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Date(1405432065579l));
-    }
+  public long getCheckInterval() {
+    return checkInterval;
+  }
 }
--- src/main/java/com/spotify/elasticsearch/plugins/indexwindow/IndexWindowPlugin.java
+++ src/main/java/com/spotify/elasticsearch/plugins/indexwindow/IndexWindowPlugin.java
@@ -22,19 +22,21 @@ import org.elasticsearch.rest.RestModule;
 
 public class IndexWindowPlugin extends AbstractPlugin {
 
-    @Override
-    public String name() {
-        return "index-window";
-    }
+  @Override
+  public String name() {
+    return "index-window";
+  }
 
-    @Override
-    public String description() {
-        return "Helps defining a fixed time-based window of indices. It assumes that time is encoded as postfix in name of the index. It continuesly removes the indices that are out of the time window.";
-    }
+  @Override
+  public String description() {
+    return "Helps defining a fixed time-based window of indices. It assumes that time is encoded as postfix in name "
+           + "of the index. It continuesly removes the indices that are out of the time window.";
+  }
 
-    @Override
-    public void processModule(Module module) {
-        if (module instanceof RestModule)
-            ((RestModule) module).addRestAction(IndexWindowAction.class);
+  @Override
+  public void processModule(Module module) {
+    if (module instanceof RestModule) {
+      ((RestModule) module).addRestAction(IndexWindowAction.class);
     }
+  }
 }
